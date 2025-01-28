31e91d589fb129402f718d8274455ed54128f0b6
Christopher-OOC <olojedechristopher24@gmail.com>
2025-01-28 22:05:10 +0100

    djhd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import DateCounter from "./DateCounter";
+import Main from "./Main";
+import Header from "./Header";
 import "./index.css";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
